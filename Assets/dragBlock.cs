a80ffc96fea0b5d6811bd42d743117c3ca8e4d74
shunhuaiyao <shunhuai.yao@gmail.com>
2017-10-28 23:23:27 +0800

    create prototype

---
+++ Assets/dragBlock.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class dragBlock : MonoBehaviour {
+
+    private Vector3 dragPosition;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = dragPosition;
+	}
+
+    void OnMouseDrag()
+    {
+        dragPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+        dragPosition = Camera.main.ScreenToWorldPoint(dragPosition);
+    }
+}
