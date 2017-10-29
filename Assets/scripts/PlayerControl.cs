114541086a3e791be3db09a0e3e63f7affe14066
shunhuaiyao <shunhuai.yao@gmail.com>
2017-10-29 15:00:59 +0800

    Jean: position changed and fix player position

---
+++ Assets/scripts/PlayerControl.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(Input.mousePosition.x, Input.mousePosition.y, 0);
+
+    }
+}
