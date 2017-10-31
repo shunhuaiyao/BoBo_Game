08e06286502d34d55c275339deb5b14b8f7725be
Jean Liu <jean4599@gmail.com>
2017-10-31 16:46:17 +0800

    Blocks are restricted to discrete one directional movement

--- Assets/scripts/dragBlock.cs
+++ Assets/scripts/dragBlock.cs
@@ -15,9 +15,22 @@ public class DragBlock : MonoBehaviour {
 	}
 
 	void OnMouseDrag(){
-		Vector3 mousePoistion = new Vector3 (Input.mousePosition.x, Input.mousePosition.y, 10);
-		Vector3 blockPosition = Camera.main.ScreenToWorldPoint (mousePoistion);
-		transform.position = new Vector3(blockPosition.x, blockPosition.y, 0.0f);
+		
+		float vx = Input.GetAxis ("Mouse X");
+		float vy = Input.GetAxis ("Mouse Y");
 
+
+		if (Mathf.Abs(vx) > Mathf.Abs(vy)) {
+			Debug.Log(Mathf.Abs(vx));
+			Vector3 mousePoistion = new Vector3 (Input.mousePosition.x, 10, 10);
+			Vector3 blockPosition = Camera.main.ScreenToWorldPoint (mousePoistion);
+			Debug.Log (blockPosition);
+			transform.position = new Vector3(Mathf.Floor(blockPosition.x), transform.position.y, 0.0f);
+		}
+		else {
+			Vector3 mousePoistion = new Vector3 (10, Input.mousePosition.y, 10);
+			Vector3 blockPosition = Camera.main.ScreenToWorldPoint (mousePoistion);
+			transform.position = new Vector3(transform.position.x, Mathf.Floor(blockPosition.y), 0.0f);
+		}
 	}
 }
